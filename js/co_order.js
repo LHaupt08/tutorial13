ea2988aa21a9f7a96c4109d5c926dd8667bd1ec1
LHaupt1s <LHaupt1s@capetigers.com>
2026-03-09 10:40:45 -0500

    page 990

--- js/co_order.js
+++ js/co_order.js
@@ -28,4 +28,55 @@
    
 */
 
+window.addEventListener("load", function() {
+   var orderForm = this.document.forms.orderForm;
+   orderForm.elements.orderDate.value = new Date().toDateString();
+   orderForm.elements.model.focus();
 
+   // Calculate cost of the order
+   calcOrder();
+});
+
+function calcOrder() {
+
+   var orderForm = document.forms.orderForm;
+
+   // Calculate the initial cost of the order
+   var mIndex = orderForm.elements.model.selectedIndex;
+   var mCost = orderForm.elements.model.options[mIndex].value;
+   var qIndex = orderForm.elements.qty.selectedIndex;
+   var quantity =  orderForm.elements.qty[qIndex].value;
+
+   // Initial cost = model cost x quantity
+
+   var initialCost = mCost*quantity;
+   orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
+
+   // Retrieve the cost of the user's protection plan
+
+   var pCost = document.querySelector('input[name="protection"]:checked').value*quantity;
+   orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
+
+   // Calculate the order subtotal
+   orderForm.elements.subtotal.value =  formatNumber(initialCost + pCost, 2);
+
+   // Calculate the sales tax
+   var salesTax = 0.05*(initialCost + pCost);
+   orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+   // Calculate the cost of the total order
+   var totalCost = initialCost + pCost + salesTax;
+   orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
+}
+
+function formatNumber(val, decimals) {
+   return val.toLocaleString(undefined,
+      {minimumFractionDidgets:decimals, maximumFractionDidgets:decimals}
+   );
+}
+
+function formatUSCurrency(val) {
+   return val.toLocaleString('en-US',
+      {style:"currency", currency: "USD"}
+   );
+}
